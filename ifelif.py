842356d1785be0d6c6a15f43c3b7469ed466869d
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-01-14 17:06:38 -0300

    Add files via upload

---
+++ ifelif.py
@@ -0,0 +1,10 @@
+age = int(input("what's your age?"))
+
+if age <= 15:
+    print("you are younger than sixteen")
+elif age == 16:
+    print("You are 16")
+elif age == 17:
+    print("you are 17")
+else:
+    print("You are older than 16")
