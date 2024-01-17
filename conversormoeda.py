66eea695ef83627ae3700db31f87291a8aec023e
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-01-17 20:12:52 -0300

    Add files via upload

---
+++ conversormoeda.py
@@ -0,0 +1,7 @@
+carteira = float(input('Quantos reais eu tenho na carteira?'))
+dolar = carteira / 3.27
+
+print('Esta carteira tem {:.2f} dolares'.format(dolar) ) 
+
+#if dollar == int(carteira + 3.27):
+ #   print('Esta carteira tem {} dolares' )
