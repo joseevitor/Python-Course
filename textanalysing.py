566feaf35c51a051662c9e35a3327d9b3e187721
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-01-27 19:27:02 -0300

    Add files via upload

--- textanalysing.py
+++ textanalysing.py
@@ -4,7 +4,7 @@ print('Your name with uppercase letters is {}'.format(phrase.upper()))
 
 print('Your name with lowercase letters is {}'.format(phrase.lower()))
 
-first_name = phrase.split()[0]
-print('The first name is {}'.format(len(first_name)))
+name = phrase.split()
+print('The first name is {}, and it has {} letters'.format((name[0]),len(name[0])))
 
 print('Your name has {} letters'.format(len(phrase)-phrase.count(' ')))
