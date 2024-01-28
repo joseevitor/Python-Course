66fb8a88633d62783265f404a06c729dbf9ccc71
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-01-27 23:09:59 -0300

    Add files via upload

---
+++ exstring.py
@@ -0,0 +1,5 @@
+# create a phrase that could read  the citys name and return if it beggining or not with "Santo"
+
+city = str(inpu('Tell me your city: ')).strip()
+
+print(city[:5].upper) == 'SANTO'
