3ead465d8fbcd814400e31a1fb57023ab44e1f2e
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-01-27 21:13:05 -0300

    Add files via upload

---
+++ separatingthedigitsofthenumber.py
@@ -0,0 +1,10 @@
+number = int(input('Tell which number you wnat: '))
+
+
+n = str(number)
+
+print('Analysing the number {}'.format(number))
+print('Unity: {}'.format(n[3]))
+print('Ten: {}'.format(n[2]))
+print('Hundred: {}'.format(n[1]))
+print('Thousend: {}'.format(n[0]))
