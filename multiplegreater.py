1388bcf779cccfe3ff3aa4f35898e8e47c676617
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-07 23:41:38 -0300

    Add files via upload

---
+++ multiplegreater.py
@@ -0,0 +1,8 @@
+salary = float(input('What is the clerk salary? '))
+
+if salary <= 1250:
+    new = salary + (salary * 15/100)
+else:
+    new = salary + (salary * 10/100)
+    
+print('Who received R${:.2f} now will gain/earn R${:.2f}'.format(salary, new))
