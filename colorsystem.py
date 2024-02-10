f6d193c48fdbb6642967f17516682b4a5756f161
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-10 08:44:20 -0300

    Add files via upload

---
+++ colorsystem.py
@@ -0,0 +1,9 @@
+print('\033[30;45mHello world\033[m')
+print('=-=-'*20)
+print('\033[7;30;44mHello world\033[m')
+
+print('=-=-'*20)
+
+a = 3
+b = 5
+print('The values are \033[32m{}\033[m and \033[31m{}'.format(a, b))
