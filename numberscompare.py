e591179f93dbc68bbea3cf4c06fe8b15e852ae11
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-13 22:16:40 -0300

    Add files via upload

---
+++ numberscompare.py
@@ -0,0 +1,11 @@
+firsdnumber = float(input('Tell me the first number: '))
+second = float(input('Tell me the second number: '))
+
+#print()
+
+if firsdnumber > second:
+    print('The firt number is bigger')
+elif second > firsdnumber:
+    print('The second number is bigger')
+else:
+    print('The both numbers are the same')
