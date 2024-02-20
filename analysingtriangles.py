4e11fc64c930267f667b6adda184b4b8908bd96c
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-19 23:54:19 -0300

    Add files via upload

--- analysingtriangles.py
+++ analysingtriangles.py
@@ -3,7 +3,7 @@ secondlinesegment = float(input('Tell me the value of the second line segment: '
 thirdlinesegment = float(input('Tell me the value of the third line segment: '))
 
 if firstlinesegment == secondlinesegment == thirdlinesegment:
-    print('This is an EQUILATERAL TRIANGLE!! ')
+    print('This is an EQUILATERAL TRIANGLE!!')
 elif firstlinesegment == secondlinesegment > thirdlinesegment or firstlinesegment == thirdlinesegment > secondlinesegment or secondlinesegment == thirdlinesegment > firstlinesegment:
     print('This is an ISOSCELES TRIANGLE!!!')
 elif firstlinesegment == secondlinesegment + thirdlinesegment or secondlinesegment == firstlinesegment + thirdlinesegment or thirdlinesegment == firstlinesegment + secondlinesegment:
