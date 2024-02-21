76157dc6b5333d82b322a13c8e3396ec99c33a27
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-20 23:32:14 -0300

    Add files via upload

--- analysingtriangles.py
+++ analysingtriangles.py
@@ -2,6 +2,7 @@ firstlinesegment = float(input('Tell me the value of the first line segment: '))
 secondlinesegment = float(input('Tell me the value of the second line segment: '))
 thirdlinesegment = float(input('Tell me the value of the third line segment: '))
 
+
 if firstlinesegment == secondlinesegment == thirdlinesegment:
     print('This is an EQUILATERAL TRIANGLE!!')
 elif firstlinesegment == secondlinesegment > thirdlinesegment or firstlinesegment == thirdlinesegment > secondlinesegment or secondlinesegment == thirdlinesegment > firstlinesegment:
