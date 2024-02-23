8c5a911509c9040ea87c437037bab375539e1cd1
joseevitor <121908337+joseevitor@users.noreply.github.com>
2024-02-22 23:04:48 -0300

    Add files via upload

---
+++ bodymass.py
@@ -0,0 +1,15 @@
+weight = float(input('What is your weight? (Kg)' ))
+
+height = float(input('Whats is your height? (m)'))
+
+MBI = weight / (height ** 2)
+
+print('The mass body index of this person is {:.1f}'.format(MBI))
+
+if MBI < 18.5:
+    print('The person has desnutrition! Take care of your body')
+
+if 18.5 <= MBI <= 25:
+    print('The person has de ideal weight! Good job')
+
+if 
